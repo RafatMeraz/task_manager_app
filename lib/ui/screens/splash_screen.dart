8be75c81cb1196accf8b75dd94c80ca69bb5ecbb
RafatMeraz <razhossain201@gmail.com>
2025-11-01 23:04:42 +0600

    Initial commit

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager_app/ui/screens/sign_in_screen.dart';
+import 'package:task_manager_app/ui/utils/asset_paths.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => SignInScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            AssetPaths.backgroundSvg,
+            width: double.maxFinite,
+            height: double.maxFinite,
+            fit: BoxFit.cover,
+          ),
+          Center(child: SvgPicture.asset(AssetPaths.logoSvg, width: 120)),
+        ],
+      ),
+    );
+  }
+}
