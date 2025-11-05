284668c9eaa39ecc5620baa7f0c4f38297bdb925
RafatMeraz <razhossain201@gmail.com>
2025-11-05 22:52:37 +0600

    Add reset password screen ui design

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/ui/screens/forgot_password_email_screen.dart';
 import 'package:task_manager_app/ui/screens/forgot_password_verify_otp_screen.dart';
+import 'package:task_manager_app/ui/screens/reset_password_screen.dart';
 import 'package:task_manager_app/ui/screens/sign_in_screen.dart';
 import 'package:task_manager_app/ui/screens/sign_up_screen.dart';
 import 'package:task_manager_app/ui/screens/splash_screen.dart';
@@ -62,7 +63,8 @@ class TaskManagerApp extends StatelessWidget {
         SignInScreen.name : (_) => SignInScreen(),
         SignUpScreen.name : (_) => SignUpScreen(),
         ForgotPasswordEmailScreen.name : (_) => ForgotPasswordEmailScreen(),
-        ForgotPasswordVerifyOtpScreen.name : (_) => ForgotPasswordVerifyOtpScreen()
+        ForgotPasswordVerifyOtpScreen.name : (_) => ForgotPasswordVerifyOtpScreen(),
+        ResetPasswordScreen.name : (_) => ResetPasswordScreen()
       },
       initialRoute: SplashScreen.name,
     );
--- lib/ui/screens/reset_password_screen.dart
+++ lib/ui/screens/reset_password_screen.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
-import 'package:task_manager_app/ui/screens/reset_password_screen.dart';
+import 'package:task_manager_app/ui/screens/forgot_password_verify_otp_screen.dart';
 import 'package:task_manager_app/ui/screens/sign_in_screen.dart';
 import 'package:task_manager_app/ui/widgets/screen_background.dart';
 
-class ForgotPasswordVerifyOtpScreen extends StatefulWidget {
-  const ForgotPasswordVerifyOtpScreen({super.key});
+class ResetPasswordScreen extends StatefulWidget {
+  const ResetPasswordScreen({super.key});
 
-  static const String name = '/forgot-password-verify-otp';
+  static const String name = '/reset-password';
 
   @override
-  State<ForgotPasswordVerifyOtpScreen> createState() =>
-      _ForgotPasswordVerifyOtpScreenState();
+  State<ResetPasswordScreen> createState() => _ResetPasswordScreenState();
 }
 
-class _ForgotPasswordVerifyOtpScreenState
-    extends State<ForgotPasswordVerifyOtpScreen> {
+class _ResetPasswordScreenState extends State<ResetPasswordScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,37 +26,24 @@ class _ForgotPasswordVerifyOtpScreenState
             children: [
               const SizedBox(height: 60),
               Text(
-                'OTP Verification',
+                'Reset Password',
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               Text(
-                'A 6 digits verification OTP has been sent to your email address',
+                'Minimum length of password should more than 8 letters',
                 style: Theme.of(context).textTheme.labelMedium,
               ),
               const SizedBox(height: 8),
-              PinCodeTextField(
-                length: 6,
-                obscureText: false,
-                animationType: AnimationType.fade,
-                keyboardType: TextInputType.number,
-                pinTheme: PinTheme(
-                  shape: PinCodeFieldShape.box,
-                  borderRadius: BorderRadius.circular(5),
-                  fieldHeight: 50,
-                  fieldWidth: 40,
-                  activeFillColor: Colors.white,
-                  inactiveFillColor: Colors.white,
-                  selectedFillColor: Colors.white,
-                ),
-                animationDuration: Duration(milliseconds: 300),
-                backgroundColor: Colors.transparent,
-                enableActiveFill: true,
-                appContext: context,
+              TextFormField(
+                decoration: InputDecoration(hintText: 'New Password'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Confirm Password'),
               ),
               const SizedBox(height: 8),
               FilledButton(
-                onPressed: _onTapVerifyButton,
-                child: Text('Verify'),
+                onPressed: _onTapConfirmButton,
+                child: Text('Confirm'),
               ),
               const SizedBox(height: 24),
               Center(
@@ -90,10 +74,11 @@ class _ForgotPasswordVerifyOtpScreenState
 
   void _onTapSignInButton() {
     Navigator.pushNamedAndRemoveUntil(
-        context, SignInScreen.name, (predicate) => false);
+      context,
+      SignInScreen.name,
+      (predicate) => false,
+    );
   }
 
-  void _onTapVerifyButton() {
-    Navigator.pushNamed(context, ResetPasswordScreen.name);
-  }
+  void _onTapConfirmButton() {}
 }
