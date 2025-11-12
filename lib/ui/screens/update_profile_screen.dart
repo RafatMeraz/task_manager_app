06c491ba446b21f273af137c1da7c38799aae5dd
RafatMeraz <razhossain201@gmail.com>
2025-11-12 22:25:28 +0600

    Complete update profile screen ui

--- lib/ui/screens/update_profile_screen.dart
+++ lib/ui/screens/update_profile_screen.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_app/ui/widgets/screen_background.dart';
 import 'package:task_manager_app/ui/widgets/tm_app_bar.dart';
+
+import '../widgets/photo_picker.dart';
 
 class UpdateProfileScreen extends StatefulWidget {
   const UpdateProfileScreen({super.key});
@@ -16,7 +17,7 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TMAppBar(), // TODO: Fix repeating same screen
+      appBar: TMAppBar(fromUpdateProfile: true),
       body: ScreenBackground(
         child: Padding(
           padding: const EdgeInsets.all(24),
@@ -30,15 +31,17 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               const SizedBox(height: 8),
-              // TODO: Add select photo section
+              GestureDetector(onTap: () {}, child: PhotoPicker()),
               TextFormField(decoration: InputDecoration(hintText: 'Email')),
-              TextFormField(decoration: InputDecoration(hintText: 'First name')),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'First name'),
+              ),
               TextFormField(decoration: InputDecoration(hintText: 'Last name')),
               TextFormField(decoration: InputDecoration(hintText: 'Mobile')),
               TextFormField(decoration: InputDecoration(hintText: 'Password')),
               const SizedBox(height: 8),
               FilledButton(
-                onPressed: _onTapSignUpButton,
+                onPressed: _onTapUpdateButton,
                 child: Icon(Icons.arrow_circle_right_outlined),
               ),
             ],
@@ -48,9 +51,5 @@ class _UpdateProfileScreenState extends State<UpdateProfileScreen> {
     );
   }
 
-  void _onTapSignInButton() {
-    Navigator.pop(context);
-  }
-
-  void _onTapSignUpButton() {}
+  void _onTapUpdateButton() {}
 }
