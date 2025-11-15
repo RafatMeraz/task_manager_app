655ebd4000c1c364c11c619e300f41e301eb544a
RafatMeraz <razhossain201@gmail.com>
2025-11-15 22:57:44 +0600

    Integrate sign in api

--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,9 +1,13 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/data/service/network_caller.dart';
+import 'package:task_manager_app/data/utils/urls.dart';
 import 'package:task_manager_app/ui/screens/forgot_password_email_screen.dart';
 import 'package:task_manager_app/ui/screens/main_bottom_nav_holder_screen.dart';
 import 'package:task_manager_app/ui/screens/sign_up_screen.dart';
 import 'package:task_manager_app/ui/widgets/screen_background.dart';
+import 'package:task_manager_app/ui/widgets/snack_bar_message.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -15,58 +19,98 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _signInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
         child: Padding(
           padding: const EdgeInsets.all(24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            spacing: 8,
-            children: [
-              const SizedBox(height: 60),
-              Text(
-                'Get Started With',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(height: 8),
-              TextFormField(decoration: InputDecoration(hintText: 'Email')),
-              TextFormField(decoration: InputDecoration(hintText: 'Password')),
-              const SizedBox(height: 8),
-              FilledButton(
-                onPressed: _onTapSignInButton,
-                child: Icon(Icons.arrow_circle_right_outlined),
-              ),
-              const SizedBox(height: 24),
-              Center(
-                child: Column(
-                  children: [
-                    TextButton(
-                      onPressed: _onTapForgotPasswordButton,
-                      child: Text('Forgot Password?'),
-                    ),
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                        ),
-                        text: "Don't have an account? ",
-                        children: [
-                          TextSpan(
-                            style: TextStyle(color: Colors.green),
-                            text: 'Sign Up',
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = _onTapSignUpButton,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 8,
+              children: [
+                const SizedBox(height: 60),
+                Text(
+                  'Get Started With',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-              ),
-            ],
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _emailTEController,
+                  decoration: InputDecoration(hintText: 'Email'),
+                  validator: (String? value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter a your email';
+                    }
+                    if (EmailValidator.validate(value!) == false) {
+                      return 'Enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordTEController,
+                  decoration: InputDecoration(hintText: 'Password'),
+                  validator: (String? value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Enter a your password';
+                    }
+                    if (value!.length < 6) {
+                      return 'Enter a password more than 6 letters';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 8),
+                Visibility(
+                  visible: _signInProgress == false,
+                  replacement: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  child: FilledButton(
+                    onPressed: _onTapSignInButton,
+                    child: Icon(Icons.arrow_circle_right_outlined),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Center(
+                  child: Column(
+                    children: [
+                      TextButton(
+                        onPressed: _onTapForgotPasswordButton,
+                        child: Text('Forgot Password?'),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          text: "Don't have an account? ",
+                          children: [
+                            TextSpan(
+                              style: TextStyle(color: Colors.green),
+                              text: 'Sign Up',
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = _onTapSignUpButton,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -74,7 +118,28 @@ class _SignInScreenState extends State<SignInScreen> {
   }
 
   void _onTapSignInButton() {
-    Navigator.pushReplacementNamed(context, MainBottomNavHolderScreen.name);
+
+    if (_formKey.currentState!.validate()) {
+      _signIn();
+    }
+  }
+
+  Future<void> _signIn() async {
+    _signInProgress = true;
+    setState(() {});
+
+    Map<String, dynamic> requestBody = {
+      "email": _emailTEController.text.trim(),
+      "password": _passwordTEController.text
+    };
+
+    final NetworkResponse response = await NetworkCaller.postRequest(
+        Urls.loginUrl, body: requestBody);
+    if (response.isSuccess) {
+      Navigator.pushReplacementNamed(context, MainBottomNavHolderScreen.name);
+    } else {
+      showSnackBarMessage(context, response.errorMessage);
+    }
   }
 
   void _onTapForgotPasswordButton() {
