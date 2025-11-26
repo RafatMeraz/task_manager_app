618a5a7361207a2ac15b7780bfddf1f338529ade
RafatMeraz <razhossain201@gmail.com>
2025-11-26 22:20:47 +0600

    Integrate get completed task list api

--- lib/ui/screens/completed_task_list_screen.dart
+++ lib/ui/screens/completed_task_list_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_app/ui/widgets/centered_circular_progress.dart';
 
+import '../../data/models/task_model.dart';
+import '../../data/service/network_caller.dart';
+import '../../data/utils/urls.dart';
+import '../widgets/snack_bar_message.dart';
 import '../widgets/task_card.dart';
 
 class CompletedTaskListScreen extends StatefulWidget {
@@ -10,18 +15,58 @@ class CompletedTaskListScreen extends StatefulWidget {
 }
 
 class _CompletedTaskListScreenState extends State<CompletedTaskListScreen> {
+  bool _getCompletedTaskListInProgress = false;
+
+  List<TaskModel> _completedTaskList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCompletedTaskList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.separated(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          // return TaskCard();
-        },
-        separatorBuilder: (context, index) {
-          return SizedBox(height: 8);
-        },
+      body: Visibility(
+        visible: _getCompletedTaskListInProgress == false,
+        replacement: CenteredCircularProgress(),
+        child: ListView.separated(
+          itemCount: _completedTaskList.length,
+          itemBuilder: (context, index) {
+            return TaskCard(
+              taskModel: _completedTaskList[index],
+              refreshList: () {
+                _getCompletedTaskList();
+              },
+            );
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 8);
+          },
+        ),
       ),
     );
+  }
+
+  Future<void> _getCompletedTaskList() async {
+    _getCompletedTaskListInProgress = true;
+    setState(() {});
+    final NetworkResponse response = await NetworkCaller.getRequest(
+      Urls.completedTasksUrl,
+    );
+
+    if (response.isSuccess) {
+      List<TaskModel> list = [];
+      for (Map<String, dynamic> jsonData in response.body['data']) {
+        list.add(TaskModel.fromJson(jsonData));
+      }
+      _completedTaskList = list;
+    } else {
+      showSnackBarMessage(context, response.errorMessage);
+    }
+
+    _getCompletedTaskListInProgress = false;
+    setState(() {});
   }
 }
